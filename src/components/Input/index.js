01f04dac0199ac704cc09b1cf6a5ac5b853f2caf
Gabriel Antero <gabrielantero2012@hotmail.com>
2020-08-20 07:13:35 -0300

    feat: Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container, Text, Input } from './styles'
+
+const InputComponent = ({ label, name, register, placeholder, error, type}) => (
+  <Container>
+    {label && <Text>{label}</Text>}
+    <Container>
+      <Input name={name} ref={register} placeholder={placeholder} error={error} type={type} />
+      <Text position='absolute' bottom={0} color='red'>
+        {error}
+      </Text>
+    </Container>
+  </Container>
+);
+
+export default InputComponent;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  border: 1px solid #424242;
+  padding: 8px;
+`;
